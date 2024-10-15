295c0a5e6c6b269d0bd670a8fbb182ca49d702e5
killiannh <killiannhervagault@gmail.com>
2024-10-15 21:25:53 +0200

    bouncing ball

---
+++ client/ball.js
@@ -0,0 +1,35 @@
+function Ball() {
+    this.pos = createVector(width / 2, height / 2);
+    this.speed = 5;
+
+    this.r = 10;
+    this.direction = createVector(1, 1);
+    this.vel = createVector(1, 1).mult(this.speed);
+
+    this.display = function() {
+        ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+    }
+
+    this.update = function() {
+        this.pos.x += this.vel.x * this.direction.x;
+        this.pos.y += this.vel.y * this.direction.y;
+    }
+
+    this.checkEdges = function() {
+        if (this.pos.y < this.r && this.direction.y < 0) {
+            this.direction.y *= -1;
+        }
+        
+        if (this.pos.y > height - this.r && this.direction.y > 0) {
+            this.direction.y *= -1;
+        }
+
+        if (this.pos.x < this.r && this.direction.x < 0) {
+            this.direction.x *= -1;
+        }
+
+        if (this.pos.x > width - this.r && this.direction.x > 0) {
+            this.direction.x *= -1;
+        }
+    }
+}
--- client/sketch.js
+++ client/sketch.js
@@ -1,8 +1,10 @@
 var paddle;
+var ball;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     paddle = new Paddle();
+    ball = new Ball();
 }
 
 function draw() {
@@ -11,7 +13,13 @@ function draw() {
     paddle.display();
     paddle.checkEdges()
     paddle.update();
+
+    ball.display();
+    ball.update();
+    ball.checkEdges();
 }
+
+
 
 function keyPressed() {
     if(key === 'ArrowLeft') {
@@ -22,3 +30,10 @@ function keyPressed() {
         paddle.isMovingLeft = false;
     }
 }
+
+function keyReleased() {
+    if(key === 'ArrowLeft' || key === 'ArrowRight') {
+        paddle.isMovingLeft = false;
+        paddle.isMovingRight = false;
+    }
+  }
